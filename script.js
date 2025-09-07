ebadcd3ec235dbc04783d7b40306889cc2aa6cd4
BinBil2205 <bintangkrn2708@gmail.com>
2025-09-07 21:05:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling dan navbar aktif otomatis
+const navLinks = document.querySelectorAll("header nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    document.querySelectorAll("section").forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
