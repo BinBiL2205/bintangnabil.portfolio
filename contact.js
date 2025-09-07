0e1ebfd79ee4a5d5abe6c2c88de86e745206c2f7
BinBil2205 <bintangkrn2708@gmail.com>
2025-09-08 01:07:43 +0700

    iya

--- contact.js
+++ contact.js
@@ -1,28 +1,42 @@
-// Tunggu sampai halaman selesai dimuat
-document.addEventListener("DOMContentLoaded", function () {
+const btn = document.getElementById('button');
+
+
     // Ambil form berdasarkan ID
-    const contactForm = document.getElementById("contact-form");
+    document.getElementById("contact-form").addEventListener
 
     // Tambahkan event listener ketika form disubmit
-    contactForm.addEventListener("submit", function (e) {
-        e.preventDefault(); // Mencegah reload halaman
+    ("submit", function(event) {
 
-        // Ambil data input dari form
-        const formData = {
-            from_name: contactForm.from_name.value,
-            reply_to: contactForm.reply_to.value,
-            message: contactForm.message.value
-        };
+        event.preventDefault(); // Mencegah reload halaman
 
-        // Kirim email menggunakan EmailJS
-        emailjs.send("service_1xxn7d6", "template_f0xmgo9", formData)
-            .then(function (response) {
-                alert("✅ Pesan berhasil dikirim! Terima kasih sudah menghubungi saya.");
-                console.log("SUCCESS!", response.status, response.text);
-                contactForm.reset(); // Reset form setelah sukses
-            }, function (error) {
-                alert("❌ Pesan gagal dikirim! Coba lagi nanti.");
-                console.error("FAILED...", error);
-            });
-    });
-});
+        btn.value = 'sending ...';
+        const serviceID ='service_1xxn7d6';
+        const templateID ='template_f0xmgo9';
+        
+        emailjs.sendform(serviceID, templateID, this)
+        then(() => {
+            btn.value = 'Send Email';
+            alert('Email berhasil Terkirim');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+
+        // // Ambil data input dari form
+        // const formData = {
+        //     from_name: contactForm.from_name.value,
+        //     reply_to: contactForm.reply_to.value,
+        //     message: contactForm.message.value
+        // };
+
+        // // Kirim email menggunakan EmailJS
+        // emailjs.send("service_1xxn7d6", "template_f0xmgo9", formData)
+        //     .then(function (response) {
+        //         alert("✅ Pesan berhasil dikirim! Terima kasih sudah menghubungi saya.");
+        //         console.log("SUCCESS!", response.status, response.text);
+        //         contactForm.reset(); // Reset form setelah sukses
+        //     }, function (error) {
+        //         alert("❌ Pesan gagal dikirim! Coba lagi nanti.");
+        //         console.error("FAILED...", error);
+        //     });
+
